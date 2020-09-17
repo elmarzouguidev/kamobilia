e2f017d8817ebb3adb52481b302956d5a53e3d47
devscript-abdo <elmarzouguiabdelghafour@gmail.com>
2020-09-17 20:37:03 +0100

    add filter to promo and news products

--- resources/views/promos/_section_header.blade.php
+++ resources/views/promos/_section_header.blade.php
@@ -3,11 +3,11 @@
         <section class="main-header" style="background-image:url(assets/images/gallery-3.jpg)">
             <header>
                 <div class="container">
-                    <h1 class="h2 title">Shop</h1>
+                    <h1 class="h2 title">Promos du mois</h1>
                     <ol class="breadcrumb breadcrumb-inverted">
                         <li><a href="{{route('home')}}"><span class="icon icon-home"></span></a></li>
-                        <li><a href="{{route('categories')}}">Product Category</a></li>
-                        <li><a class="active" href="products-grid.html">Product Sub-category</a></li>
+                        <li><a href="{{route('categories')}}">catégories</a></li>
+                        
                     </ol>
                 </div>
             </header>
